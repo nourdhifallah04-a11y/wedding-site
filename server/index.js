0bf2bc5d54a849a7a5fe0fdf161c01af232378e0
Votre Nom <votre.email@exemple.com>
2026-09-06 02:11:38 +0200

    Force IPv4 for Render SMTP

--- server/index.js
+++ server/index.js
@@ -24,6 +24,7 @@ const transporter = nodemailer.createTransport({
   port: smtpPort,
   secure: smtpPort === 465,
   requireTLS: smtpPort === 587,
+  family: 4,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS
